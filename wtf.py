6602276d13c90ab66749c5af70e1235e6aa8227d
Emily Marx <emilykmarx@alumni.stanford.edu>
2023-08-31 01:44:01 +0000

    write user id to rmq logs exchange (via rmq_publisher) after buying ticket

--- wtf.py
+++ wtf.py
@@ -2,8 +2,10 @@ import logging
 from queries import Query
 from scenarios import *
 import subprocess
+import json
 from http.client import HTTPConnection
 
+# Makes assumptions about location of rmq go files
 def verbose_requests():
   # Log lots of info on http requests
   HTTPConnection.debuglevel = 1
@@ -19,7 +21,8 @@ def main():
   cluster_ip = subprocess.check_output(cluster_ip_cmd, shell=True, text=True).strip()
   url = f"http://{cluster_ip}:32677"
   q = Query(url)
-  # login train-ticket and store the cookies
+
+  # 1. Do ticket workflow
   if not q.login():
     logging.fatal('login failed')
 
@@ -27,6 +30,12 @@ def main():
   query_and_pay(q)
   query_and_collect(q)
   query_and_execute(q)
+  user_info = json.dumps({"user_id": q.uid})
+
+  # 2. Write to rmq for logstash
+  subprocess.check_output(f"go run wtf/rmq_publisher.go -msg='{user_info}' -json",
+                          shell=True, cwd="../rabbitmq-tutorials/go", text=True)
+
 
 if __name__ == '__main__':
   main()
